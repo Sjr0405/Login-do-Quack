22b756656366704699a6b6e8d0dca66f8e6c009f
Samuel <136186613+Sjr0405@users.noreply.github.com>
2024-09-03 02:05:46 -0300

    Completei uma parte

---
+++ react-teste=ing/src/Cadastro.tsx
@@ -0,0 +1,295 @@
+import { useRef, useEffect, useState } from 'react';
+import { useForm, Controller,SubmitHandler } from "react-hook-form";
+import { TextField, Button, Grid, Box, Card, Typography } from "@mui/material";
+import InputMask from "react-input-mask"; 
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import Swal from 'sweetalert2';
+import './Cadastro.css';
+
+// Esquema de validação com Yup
+const schema = yup.object().shape({
+  name: yup.string().required("Nome é obrigatório"),
+  phone: yup.string().required("Telefone é obrigatório").min(14, "Telefone inválido"),
+  cpf: yup.string().required("CPF é obrigatório").min(14, "CPF inválido"),
+  email: yup.string().email("Email inválido").required("Email é obrigatório"),
+  confirmEmail: yup.string()
+  .oneOf([yup.ref('email'), undefined], 'Os emails devem corresponder')
+  .required("Confirmação de email é obrigatória"),
+  password: yup.string().min(8, "A senha deve ter no mínimo 8 caracteres").required("Senha é obrigatória"),
+  confirmPassword: yup.string()
+  .oneOf([yup.ref('password'), undefined], 'As senhas devem corresponder')
+  .required("Confirmação de senha é obrigatória")
+});
+
+export default function MeuFormulario() {
+  const [dataList, setDataList] = useState([]);
+  const { handleSubmit, control, formState: { errors }, reset } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const [formCardHeight, setFormCardHeight] = useState('auto');
+  const formCardRef = useRef(null);
+
+  useEffect(() => {
+    if (formCardRef.current) {
+      const current = formCardRef.current as HTMLElement;
+      setFormCardHeight(`${current.offsetHeight}px`);
+    }
+  }, [formCardRef]);
+
+  interface FormData {
+    phone: string;
+    cpf: string;
+    email: string;
+    confirmEmail: string;
+    password: string;
+    confirmPassword: string;
+  }
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+    Swal.fire({
+      icon: 'success',
+      title: 'Sucesso!',
+      text: 'Login realizado com sucesso.',
+    });
+    reset();
+  };
+
+  const handleDelete = (index: number) => {
+    const newDataList = dataList.filter((_, i) => i !== index);
+    setDataList(newDataList);
+    Swal.fire({
+      icon: 'success',
+      title: 'Deletado!',
+      text: 'Registro deletado com sucesso.',
+    });
+  };
+
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'center', mt: 5 }}>
+
+      {/* Card de apresentação */}
+      <Box sx={{ width: 700, mr: -20  , height: formCardHeight , display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+        <Card sx={{ padding: 4, backgroundColor: '#eb832e', color: 'white', height: '100%' }}>
+          <Box sx={{ display: 'flex', justifyContent: 'center', mb: 3 , mt: 20}}>
+            {/* Lugar para a logo */}
+            <img src="src/assets/Quack-logo.svg" alt="Logo" style={{ width: '100px' }} />
+          </Box>
+          {/* Texto */}
+          <Typography variant="h5" align="center" gutterBottom>
+            Bem-vindo ao Quack()
+          </Typography>
+          {/* Subtexto */}
+          <Typography variant="body1" align="center" >
+            Complete o Cadastro ao lado para começar.
+            </Typography>
+          <Typography variant="body1" align="center" >
+          sua jornada de programacão
+            </Typography>
+        </Card>
+      </Box>
+
+      {/* Formulário */}
+      <Box sx={{ flexGrow: 2 }}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Card sx={{ padding: 4, mb: 4 }}>
+            <Grid container spacing={2}>
+
+              {/* Nome */}
+              <Grid item xs={12}>
+                <Controller
+                  name="name"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Nome"
+                      fullWidth
+                      error={!!errors.name}
+                      helperText={errors.name ? errors.name.message : ""}
+                    />
+                  )}
+                />
+              </Grid>
+
+              {/* Telefone */}
+              <Grid item xs={12}>
+                <Controller
+                  name="phone"
+                  control={control}
+                  render={({ field }) => (
+                    <InputMask mask="(99) 99999-9999" value={field.value} onChange={field.onChange}>
+                      {(inputProps) => (
+                        <TextField
+                          {...inputProps}
+                          label="Telefone"
+                          fullWidth
+                          error={!!errors.phone}
+                          helperText={errors.phone ? errors.phone.message : ""}
+                        />
+                      )}
+                    </InputMask>
+                  )}
+                />
+              </Grid>
+
+              {/* CPF */}
+              <Grid item xs={12}>
+                <Controller
+                  name="cpf"
+                  control={control}
+                  render={({ field }) => (
+                    <InputMask mask="999.999.999-99" value={field.value} onChange={field.onChange}>
+                      {(inputProps) => (
+                        <TextField
+                          {...inputProps}
+                          label="CPF"
+                          fullWidth
+                          error={!!errors.cpf}
+                          helperText={errors.cpf ? errors.cpf.message : ""}
+                        />
+                      )}
+                    </InputMask>
+                  )}
+                />
+              </Grid>
+
+              {/* Email */}
+              <Grid item xs={12}>
+                <Controller
+                  name="email"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Email"
+                      fullWidth
+                      error={!!errors.email}
+                      helperText={errors.email ? errors.email.message : ""}
+                    />
+                  )}
+                />
+              </Grid>
+
+              {/* Confirmar Email */}
+              <Grid item xs={12}>
+                <Controller
+                  name="confirmEmail"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Confirmar Email"
+                      fullWidth
+                      error={!!errors.confirmEmail}
+                      helperText={errors.confirmEmail ? errors.confirmEmail.message : ""}
+                    />
+                  )}
+                />
+              </Grid>
+
+              {/* Senha */}
+              <Grid item xs={12}>
+                <Controller
+                  name="password"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Senha"
+                      type="password"
+                      fullWidth
+                      error={!!errors.password}
+                      helperText={errors.password ? errors.password.message : ""}
+                    />
+                  )}
+                />
+              </Grid>
+
+              {/* Confirmar Senha */}
+              <Grid item xs={12}>
+                <Controller
+                  name="confirmPassword"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Confirmar Senha"
+                      type="password"
+                      fullWidth
+                      error={!!errors.confirmPassword}
+                      helperText={errors.confirmPassword ? errors.confirmPassword.message : ""}
+                    />
+                  )}
+                />
+              </Grid>
+
+              {/* Botão de Enviar */}
+              <Grid item xs={12}>
+                <Button type="submit" variant="contained" color="primary" fullWidth>
+                  Enviar
+                </Button>
+              </Grid>
+
+              {/* Botão de Enviar */}
+              <Grid item xs={12}>
+                <Typography variant="body1" align="center" >
+                    Já Possui uma conta?
+                    <a className = "manolo"  href="/src/login.tsx" >
+                    Clique aqui para Entar
+                  </a>
+                  </Typography>
+              </Grid>
+
+              {/* Link de-esqueci minha senha */}
+              <Grid item xs={12}>
+                <Typography className = "manolo" align="center" variant="body2">
+                  <a href="#">Esqueceu sua senha?</a>
+                </Typography>
+              </Grid>
+
+            </Grid>
+          </Card>
+        </form>
+      </Box>
+
+      {/* Lista de Dados */}
+      {dataList.length > 0 && (
+  <Box sx={{ flexGrow: 1, ml: 2 }}>
+    <Card sx={{ padding: 4 }}>
+      <Typography variant="h6" gutterBottom>
+        Usuários Registrados
+      </Typography>
+      {dataList.map((data: { email: string; cpf: string; phone: string; name: string }, index: number) => (
+        <Box key={index} sx={{ mb: 2 }}>
+          <Typography variant="body1">
+            Nome: {data.name}
+          </Typography>
+          <Typography variant="body1">
+            Email: {data.email}
+          </Typography>
+          <Typography variant="body1">
+            CPF: {data.cpf}
+          </Typography>
+          <Typography variant="body1">
+            Telefone: {data.phone}
+          </Typography>
+          <Button
+            variant="outlined"
+            color="secondary"
+            onClick={() => handleDelete(index)}
+            sx={{ mt: 1 }}
+          >
+            Deletar
+          </Button>
+        </Box>
+      ))}
+    </Card>
+  </Box>
+)}
+
+    </Box>
+  );
+}
---
+++ react-teste=ing/src/Login.tsx
@@ -0,0 +1,137 @@
+import { useRef, useEffect, useState } from 'react';
+import { useForm, Controller,SubmitHandler } from "react-hook-form";
+import { TextField, Button, Grid, Box, Card, Typography } from "@mui/material";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import Swal from 'sweetalert2';
+import './Login.css';
+
+// Esquema de validação com Yup
+const schema = yup.object().shape({
+  email: yup.string().email("Email inválido").required("Email é obrigatório"),
+  password: yup.string().min(8, "A senha deve ter no mínimo 8 caracteres").required("Senha é obrigatória"),
+});
+
+export default function LoginForm() {
+  const { handleSubmit, control, formState: { errors }, reset } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const [formCardHeight, setFormCardHeight] = useState('auto');
+  const formCardRef = useRef(null);
+
+  useEffect(() => {
+    if (formCardRef.current) {
+      const current = formCardRef.current as HTMLElement;
+      setFormCardHeight(`${current.offsetHeight}px`);
+    }
+  }, [formCardRef]);
+
+  type FormData = {
+    email: string;
+    password: string;
+  };
+  
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+    Swal.fire({
+      icon: 'success',
+      title: 'Sucesso!',
+      text: 'Login realizado com sucesso.',
+    });
+    reset();
+  };
+  
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'center', mt: 5 }}>
+
+      {/* Card de apresentação */}
+      <Box sx={{ width: 700, height: formCardHeight, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+        <Card sx={{ mr: -5, padding: 4, backgroundColor: 'primary.main', color: 'white', height: '100%' }}>
+          <Box sx={{ display: 'flex', justifyContent: 'center', mb: 3 }}>
+            {/* Lugar para a logo */}
+            <img src="public/vite.svg" alt="Logo" style={{ width: '80px' }} />
+          </Box>
+          {/* Texto */}
+          <Typography variant="h5" align="center" gutterBottom>
+            Bem-vindo ao Quack()
+          </Typography>
+          {/* Subtexto */}
+          <Typography variant="body1" align="center">
+            Faça login para continuar.
+          </Typography>
+        </Card>
+      </Box>
+
+      {/* Formulário de Login */}
+      <Box sx={{ width: 500 }}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Card sx={{ padding: 4, mb: 4 }}>
+            <Grid container spacing={2}>
+              
+              {/* Email */}
+              <Grid item xs={12}>
+                <Controller
+                  name="email"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Email"
+                      fullWidth
+                      error={!!errors.email}
+                      helperText={errors.email ? errors.email.message : ""}
+                    />
+                  )}
+                />
+              </Grid>
+
+              {/* Senha */}
+              <Grid item xs={12}>
+                <Controller
+                  name="password"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label="Senha"
+                      type="password"
+                      fullWidth
+                      error={!!errors.password}
+                      helperText={errors.password ? errors.password.message : ""}
+                    />
+                  )}
+                />
+              </Grid>
+
+              {/* Botão de Login */}
+              <Grid item xs={12}>
+                <Button type="submit" variant="contained" color="primary" fullWidth>
+                  Entrar
+                </Button>
+              </Grid>
+
+                {/* Link de Criar conta */}
+              <Grid item xs={12}>
+                <Typography variant="body1" align="center" >
+                    Ainda não Possui uma conta?
+                    <a className = "manolo"  href="/src/login.tsx" >
+                    Clique aqui para Cadastrar-se
+                  </a>
+                  </Typography>
+              </Grid>
+
+              {/* Link de-esqueci minha senha */}
+              <Grid item xs={12}>
+                <Typography align="center" variant="body2">
+                  <a href="#">Esqueceu sua senha?</a>
+                </Typography>
+              </Grid>
+
+            </Grid>
+          </Card>
+        </form>
+      </Box>
+    </Box>
+  );
+}
